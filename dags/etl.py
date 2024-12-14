06fc3242a531fc0d90e9e0fd7ec268e1fc279bcf
nishan0000 <mdnishan006@gmail.com>
2024-12-14 23:32:40 +0530

    ETL Completed

--- dags/etl.py
+++ dags/etl.py
@@ -43,12 +43,42 @@ with DAG (
         http_conn_id = 'nasa_api', # Connection ID Defined In Airflow for NASA API
         endpoint = 'planetary/apod', # NASA API endpoint
         method = 'GET',
-        data = {"api_key":"{{ conn.nassa_api.extra_dejson.api_key }}"} # API Key from the connection
+        data = {"api_key":"{{ conn.nasa_api.extra_dejson.api_key }}"}, # API Key from the connection
+        response_filter = lambda response: response.json(), # Convert response to json
     )
 
     # Step 3: transform the data (Pick the information we need to save)
+    @task
+    def transform_apod_data(response):
+        apod_data = {
+            'title' : response.get('title', ''),
+            'explanation' : response.get('explanation', ''),
+            'url' : response.get('url', ''),
+            'date' : response.get('date', ''),
+            'media_type' : response.get('media_type', '')
+        }
+        return apod_data
 
     # Step 4: Loading Data into Postgres SQL
+    @task
+    def load_data_to_postgres(apod_data):
+        # Initialize the postgresHook (To load the data to postgres)
+        postgres_hook = PostgresHook(postgres_conn_id = 'my_postgres_connection')
+
+        # Define the SQL Insert Query
+        insert_query = """
+        INSERT INTO apod_data (title, explanation, url, date, media_type)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+
+        # Executing the SQL Query
+        postgres_hook.run(insert_query, parameters=(
+            apod_data['title'],
+            apod_data['explanation'],
+            apod_data['url'],
+            apod_data['date'],
+            apod_data['media_type']
+        ))
 
     # Step 5: verifying the data with DBViewer
 
